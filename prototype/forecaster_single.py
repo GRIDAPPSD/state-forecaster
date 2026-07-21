dff055366e629deabd46bf684a4f3f79b62c6c43
garydblack <gary.black@pnnl.gov>
2026-07-21 10:35:02 -0700

    Switch back to 60 second increment because I am now able to run non-realtime simulations again.

--- prototype/forecaster_single.py
+++ prototype/forecaster_single.py
@@ -24,8 +24,8 @@ JSON_PATH = "results_data_forecasting_13_5min_real.json"
 #JSON_PATH = "gappy_13.json"
 
 # --- streaming cadence ---
-TS_INCREMENT_SEC = 3            # timestamp spacing in the input stream (real-time)
-#TS_INCREMENT_SEC = 60           # timestamp spacing in the input stream (1 min)
+#TS_INCREMENT_SEC = 3            # timestamp spacing in the input stream (real-time)
+TS_INCREMENT_SEC = 60           # timestamp spacing in the input stream (1 min)
 #TS_INCREMENT_SEC = 300          # timestamp spacing in the input stream (5 min)
 #TS_INCREMENT_SEC = 900          # timestamp spacing in the input stream (15 min)
 
