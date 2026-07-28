e53de124de82ffcc2178c09a92bae5d65444ace7
garydblack <gary.black@pnnl.gov>
2026-07-28 11:09:26 -0700

    Input files suffix changed from json to jsonl to be more correct.

--- prototype/forecaster_single.py
+++ prototype/forecaster_single.py
@@ -18,10 +18,10 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 # =====================================================
 # CONFIG  (explicit, not implicit — designed for configurability)
 # =====================================================
-JSON_PATH = "results_data_forecasting_13.json"
-#JSON_PATH = "results_data_forecasting_13_5min.json"
-#JSON_PATH = "results_data_forecasting_13_15min.json"
-#JSON_PATH = "results_data_forecasting_123.json"
+JSON_PATH = "results_data_forecasting_13.jsonl"
+#JSON_PATH = "results_data_forecasting_13_5min.jsonl"
+#JSON_PATH = "results_data_forecasting_13_15min.jsonl"
+#JSON_PATH = "results_data_forecasting_123.jsonl"
 
 # --- streaming cadence ---
 #TS_INCREMENT_SEC = 3            # timestamp spacing in the input stream (real-time)
